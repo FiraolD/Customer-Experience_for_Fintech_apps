9dad7114d61c2e57b48c1c8f6fe3cbd5e4ce4bc1
FiraolD <firaoldelesa18@gmail.com>
2025-06-10 14:26:13 +0300

    oracle db connection made

--- db.py
+++
@@ -1,8 +0,0 @@
-﻿import oracledb
-
-conn = oracledb.connect(
-    user="sys",
-    password="Fira@0412",
-    dsn="localhost/xepdb1"
-)
-print("✅ Connected")
--- src/db_connnection.py
+++
@@ -1,7 +0,0 @@
-﻿import oracledb
-connection = oracledb.connect(
-    user="sys as sysdba",
-    password="Fira@0412",
-    dsn="localhost/XEPDB1"
-)
-print("✅ Connected")
